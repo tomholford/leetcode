ef2dc2770c15eb5959e004b6b1ed8a7086693d17
tomholford <tomholford@users.noreply.github.com>
2021-03-18 08:19:46 -0700

    206. Reverse Linked List (golang)

---
+++ reverse-linked-list/go.mod
@@ -0,0 +1,3 @@
+module leetcode.com/reverse
+
+go 1.16
---
+++ reverse-linked-list/reverseLinkedList.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+func reverseList(head *ListNode) *ListNode {
+	var stack []ListNode
+	nextNode := head
+
+	for {
+		if nextNode == nil {
+			break
+		}
+		stack = append(stack, *nextNode)
+		nextNode = nextNode.Next
+	}
+
+	var newHead *ListNode
+	var currentNode *ListNode
+
+	for len(stack) > 0 {
+		var popped ListNode
+		popped, stack = stack[len(stack)-1], stack[:len(stack)-1]
+
+		if newHead == nil {
+			newHead = &ListNode{popped.Val, nil}
+			currentNode = newHead
+			continue
+		}
+
+		currentNode.Next = &popped
+		currentNode = currentNode.Next
+		currentNode.Next = nil
+	}
+
+	return newHead
+}
+
+func main() {
+	fmt.Println("Placeholder")
+}
